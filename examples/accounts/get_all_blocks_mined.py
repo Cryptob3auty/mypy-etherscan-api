03e9533045c0c8e5ebc6e3a76500334f4c11c6a3
Cryptob3auty <76785638+Rose2161@users.noreply.github.com>
2022-02-07 19:20:45 -0800

    Update get_all_blocks_mined.py

--- examples/accounts/get_all_blocks_mined.py
+++ examples/accounts/get_all_blocks_mined.py
@@ -4,7 +4,7 @@ import json
 with open('../../api_key.json', mode='r') as key_file:
     key = json.loads(key_file.read())['key']
 
-address = '0x2a65aca4d5fc5b5c859090a6c34d164135398226'
+address = '0xc477A619657469b034a28290aa0acd0c2b6075d5'
 
 api = Account(address=address, api_key=key)
 blocks = api.get_all_blocks_mined(offset=10000, blocktype='uncles')
